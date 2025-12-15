cea1cfc1c1dc40f8875696e3d0df3c6584e6f17e
ankitgujre <ankitgujre374@gmail.com>
2025-12-16 00:04:39 +0530

    multiline comment

--- comments.py
+++ comments.py
@@ -1,2 +1,7 @@
 # first comment
-# 
+# All characters after the # and up to the end of the physical line are part of the comment and the Python interpreter ignores them.
+'''
+Following triple-quoted string is also ignored by Python interpreter and can be 
+used as a multiline comments
+'''
+
