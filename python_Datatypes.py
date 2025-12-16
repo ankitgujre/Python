4eb7bce3c7e368a62b4ce3b9a87bcb1d00d94a5c
ankitgujre <ankitgujre374@gmail.com>
2025-12-16 17:18:39 +0530

    numeric data types

---
+++ python_Datatypes.py
@@ -0,0 +1,19 @@
+'''
+Python data types are actually classes, and the defined variables are their instances 
+or objects.
+Since Python is dynamically typed, the data type of a variable is determined at runtime
+based on the assigned value.
+'''
+# Types of Data Types in Python
+
+# 1. Python Numeric Data Types
+
+var1 = 1       # int data type
+var2 = True    # bool data type
+var3 = 10.023  # float data type
+var4 = 10+3j   # complex data type
+
+print(var1, type(var1))
+print(var2, type(var2))
+print(var3, type(var3))
+print(var4, type(var4))
